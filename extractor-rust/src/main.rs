aac31133e83c442273d366cd645bd94592107a4d
boreq <boreq@0x46.net>
2025-05-06 15:09:48 +0200

    Cleanup the background

--- extractor-rust/src/main.rs
+++ extractor-rust/src/main.rs
@@ -1,15 +1,21 @@
 #![feature(duration_constructors)]
 
-use std::process::Command;
+use std::{collections::HashSet, process::Command};
 
 use env_logger::Env;
 use extractor_rust::{
     errors::Result,
     extractor::{Background, Markers, XY, YUV, flood_fill},
 };
-use image::{GenericImageView, ImageReader, Rgba};
+use image::{GenericImageView, ImageReader, Pixel, Rgba, imageops::crop};
 use log::info;
 use tempfile::TempDir;
+
+const INITIAL_CROP_FACTOR: f32 = 0.1; // 10%;
+const BACKGROUND_CLEANUP_FACTOR: f32 = 0.05; // If a group of non-transparent pixels constitutes
+// less than 1% of the image it will be made
+// transparent.
+const TRANSPARENT: Rgba<u8> = Rgba([0, 0, 0, 0]);
 
 fn main() -> Result<()> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
@@ -17,6 +23,9 @@ fn main() -> Result<()> {
     info!("Opening image...");
     let img = ImageReader::open("yellow.jpg")?.decode()?;
     let mut img = img.to_rgba8();
+
+    info!("Writing preview image...");
+    img.save("stage0.png")?;
 
     info!("Locating markers...");
     let markers = Markers::find(&img)?;
@@ -40,7 +49,7 @@ fn main() -> Result<()> {
         //yuv.y() < 0.5 && yuv.u().abs() < 0.1 && yuv.v().abs() < 0.1 }
     });
     for pixel in pixels {
-        img.put_pixel(pixel.x(), pixel.y(), Rgba([0, 0, 0, 0]));
+        img.put_pixel(pixel.x(), pixel.y(), TRANSPARENT);
     }
 
     info!("Coloring markers...");
@@ -56,6 +65,9 @@ fn main() -> Result<()> {
     let tmp_dir = TempDir::new()?;
     let magick_input = tmp_dir.path().join("stage1.png");
     let magick_output = tmp_dir.path().join("stage2.png");
+
+    info!("Writing preview image...");
+    img.save("stage1.png")?;
 
     info!("Writing image...");
     img.save(&magick_input)?;
@@ -93,7 +105,7 @@ fn main() -> Result<()> {
         .output()?;
 
     let img = ImageReader::open(magick_output)?.decode()?;
-    let img = img.to_rgba8();
+    let mut img = img.to_rgba8();
 
     //background
     //    .top_left()
@@ -108,8 +120,62 @@ fn main() -> Result<()> {
     //    .bottom_right()
     //    .color(&mut img, &background.bottom_right_color().rgb());
 
-    info!("Writing image...");
-    img.save("empty.png")?;
+    info!("Writing preview image...");
+    img.save("stage2.png")?;
+
+    info!("Cropping...");
+    let width = img.width();
+    let height = img.height();
+
+    let img = crop(
+        &mut img,
+        (width as f32 * INITIAL_CROP_FACTOR) as u32,
+        (height as f32 * INITIAL_CROP_FACTOR) as u32,
+        (width as f32 * (1.0 - 2.0 * INITIAL_CROP_FACTOR)) as u32,
+        (height as f32 * (1.0 - 2.0 * INITIAL_CROP_FACTOR)) as u32,
+    );
+    let mut img = img.to_image();
+
+    info!("Writing preview image...");
+    img.save("stage3.png")?;
+
+    info!("Cleaning up background...");
+    let mut skip: HashSet<XY> = HashSet::new();
+
+    for ix in 0..img.width() {
+        for iy in 0..img.height() {
+            let xy = XY::new(ix, iy);
+
+            if skip.contains(&xy) {
+                continue;
+            }
+
+            let color = img.get_pixel(xy.x(), xy.y());
+            if color.to_rgba() == TRANSPARENT {
+                continue;
+            }
+
+            let pixels = flood_fill(&img, xy, |xy: &XY, yuv: &YUV| {
+                let color = img.get_pixel(xy.x(), xy.y());
+                color.to_rgba() != TRANSPARENT
+            });
+
+            if (pixels.len() as f32)
+                < ((img.width() * img.height()) as f32 * BACKGROUND_CLEANUP_FACTOR)
+            {
+                for pixel in &pixels {
+                    img.put_pixel(pixel.x(), pixel.y(), TRANSPARENT);
+                }
+            }
+
+            for pixel in &pixels {
+                skip.insert(pixel.clone());
+            }
+        }
+    }
+
+    info!("Writing preview image...");
+    img.save("stage4.png")?;
 
     Ok(())
 }
